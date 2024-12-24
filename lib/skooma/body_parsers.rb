2dcd0decc0c4fbb43a4f521c22f5b523ad971af9
Svyatoslav Kryukov <me@skryukov.dev>
2024-12-24 12:58:43 +0300

    Support fallback parsers for vendor-specific media types

--- lib/skooma/body_parsers.rb
+++ lib/skooma/body_parsers.rb
@@ -6,15 +6,37 @@ module Skooma
       DEFAULT_PARSER = ->(body, **_options) { body }
 
       def [](media_type)
-        parsers[media_type.to_s.strip.downcase] || DEFAULT_PARSER
+        key = normalize_media_type(media_type)
+        parsers[key] ||
+          find_suffix_parser(key) ||
+          find_fallback_parser(key) ||
+          DEFAULT_PARSER
       end
 
       attr_accessor :parsers
 
       def register(*media_types, parser)
         media_types.each do |media_type|
-          parsers[media_type.to_s.strip.downcase] = parser
+          parsers[normalize_media_type(media_type)] = parser
         end
+      end
+
+      private
+
+      def normalize_media_type(media_type)
+        media_type.to_s.strip.downcase
+      end
+
+      def find_suffix_parser(media_type)
+        return unless media_type.include?("+")
+
+        suffix = media_type.split("+").last
+        parsers["application/#{suffix}"]
+      end
+
+      def find_fallback_parser(media_type)
+        type = media_type.split("/").first
+        parsers["#{type}/*"] || parsers["*/*"]
       end
     end
     self.parsers = {}
@@ -27,6 +49,5 @@ module Skooma
       end
     end
     register "application/json", JSONParser
-    register "application/vnd.api+json", JSONParser
   end
 end
